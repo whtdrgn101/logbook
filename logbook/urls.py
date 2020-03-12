a3386427755d43111bdb6f24cb8f7779dc4f292c
Timothy A. DeWees <timothy.dewees@gmail.com>
2020-03-11 21:43:22 -0400

    Main project commmit

---
+++ logbook/urls.py
@@ -0,0 +1,26 @@
+"""logbook URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include, re_path
+from scorecard.views.site import HomeView
+
+urlpatterns = [
+    path('', HomeView.as_view(), name="home"),
+    path('scorecard/', include('scorecard.urls')),
+    path('admin/', admin.site.urls),
+    re_path(r'^oidc/', include('mozilla_django_oidc.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+]
